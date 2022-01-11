ded82b0fed5cf6423c3435b9798313a327e6fa5a
cks3066 <cks3066@naver.com>
2022-01-12 03:22:00 +0900

    feat: ListView
    - 스크롤바 생김
    - 스크롤 위치 감시 가능
    - 메모리 절약

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: ShopItem(),
+        body: ListView(
+          children: [
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+            Text("안녕"),
+          ],
+        ),
       )
-    );
-  }
-}
-
-// 평생 안바뀌는 것만 변수에 담아서 사용
-var a = SizedBox(
-  child: Text("안녕"),
-);
-
-// 재사용이 많은 것만 커스텀 위젯으로 만들어서 사용
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        child: Text("안녕"),
     );
   }
 }
